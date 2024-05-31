d6f010d012cee1c268870fcc59db6ff886d5e6d5
Shakinthan Sivalingam <144006288+shsi003@users.noreply.github.com>
2024-05-31 06:32:11 +0200

    finished adpages

--- js/orderPage.js
+++ js/orderPage.js
@@ -34,44 +34,6 @@ async function fetchData(){
 }
 
 /* 
-<h1>Get a free Pokèmon keychain with your order</h1>
 
-		<div class="Pokedex">
-
-			<input type="text" class="pokeSearch" id="pokemonName" placeholder="Enter Pokemon name">
-		<button onclick="fetchData()">Fetch pokemon</button> <br>
-		<div class="Attribute">
-			<h1>Name</h1>
-			<p id="pokemonSpecies"> </p>
-		</div>
-		<br>
-		<div class="Attribute">
-			<h1>Appearance</h1>
-			<img src=""  class="pokemonImg" alt="pokemonSprite" id="pokemonSprite" style="display: none">
-		</div>
-
-		<div class="wrapper">
-			<form action="" class="form">
-
-				<label for="First_name">First Name</label>
-				<input type="text" required>
-				<br>
-	
-				<label for="Last_name">Last Name</label>
-				<input type="text" required>
-				<br>
-	
-				<label for="Phone_number">Phone number</label>
-				<input type="number" required>
-	
-				<label for="Delivery_adress">Delivery adress</label>
-				<input type="text" required>
-	
-				<p>Note: The bill will be sent along with the package</p>
-	
-				<button type="submit" onclick="alert('Your order has been registered')">Order</button>
-			</form>
-
-		</div>
 
 */
