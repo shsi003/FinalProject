c6b05bb54cae9b938cddca0766eb2d14e4f83502
Shakinthan Sivalingam <144006288+shsi003@users.noreply.github.com>
2024-08-15 18:06:16 +0200

    first try at search function

--- js/api.js
+++ js/api.js
@@ -1,13 +1,27 @@
 export async function fetchBooks(){
 	try{
-		const resonse = await fetch(GET https://www.googleapis.com/books/v1/categories
+		const resonse = await fetch( 'https://www.googleapis.com/books/v1/categories'
 		);
 		if(!response.ok){
-			throw new error(error);
+			throw new Error(error);
 		}
 		const books = await response.json();
 		return books;
 	}catch(error){
-		console.error('Error fetching books:', error)
+		console.error('Error fetching categories:', error)
+	};	
+
+}
+
+export async function searchBooks(query) {
+	try {
+		const resonse = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}`);
+		if(!response.ok) {
+			throw new Error(error)
+		}
+		const data = await response.json();
+		return data.items;
+	}catch(error){
+		console.error('Error fetching books:', error);
 	}
 }
