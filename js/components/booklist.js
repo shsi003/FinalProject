528797193a539e62ac1487554345960ab73db7fd
Shakinthan Sivalingam <144006288+shsi003@users.noreply.github.com>
2024-08-15 22:16:44 +0200

    added search functions for books using fetch

--- js/components/booklist.js
+++ js/components/booklist.js
@@ -7,3 +7,18 @@ import {
 	where
 } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
+const GOOGLE_BOOKS_API = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+export const searchBooks = async (query) => {
+	try {
+		const respone = await fetch(`${GOOGLE_BOOKS_API}${encodeURIComponent(query)}`);
+		const data = await response.json();
+		return data.items || [];
+	}catch(error){
+		console.error("Error fetching books:", error);
+		alert("failed to fetch books, please try again or come back later if error persists");
+		return [];
+	}
+
+};
+
